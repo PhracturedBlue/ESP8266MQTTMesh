dfb23fbf18c23640321734565e9f69efe495c076
PhracturedBlue <6xtc2h7upj@snkmail.com>
2017-04-12 13:42:37 -0700

    Add licence

--- examples/ESP8266MeshSensor/ESP8266MeshSensor.ino
+++ examples/ESP8266MeshSensor/ESP8266MeshSensor.ino
@@ -1,6 +1,21 @@
 /*
- HLW8012 code Copyright (C) 2016-2017 by Xose Pérez <xose dot perez at gmail dot com>
-*/
+ *  Copyright (C) 2016 PhracturedBlue
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  HLW8012 code Copyright (C) 2016-2017 by Xose Pérez <xose dot perez at gmail dot com>
+ */
 
 /* Sonoff POW w/ DS18B20 attached to GPIO2(SDA) */
 #define GREEN_LED   15 //MTDO
