f40ac411a1f638a9e35aa809d590b97b7fcb85e7
PhracturedBlue <6xtc2h7upj@snkmail.com>
2017-06-08 06:08:20 -0700

    Basic Hello World examole

---
+++ examples/ESP8266MeshHelloWorld/ESP8266MeshHelloWorld.ino
@@ -0,0 +1,90 @@
+#include "credentials.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266MQTTMesh.h>
+#include <FS.h>
+
+
+#if 0
+    //DO NOT Remove this section!  It is used to help the makefile find libraries
+    #include <PubSubClient.h>
+#endif
+
+
+
+#define      FIRMWARE_ID        0x1337
+#define      FIRMWARE_VER       "0.1"
+const char*  networks[]       = NETWORK_LIST;
+const char*  network_password = NETWORK_PASSWORD;
+const char*  mesh_password    = MESH_PASSWORD;
+const char*  base_ssid        = BASE_SSID;
+const char*  mqtt_server      = MQTT_SERVER;
+const int    mqtt_port        = MQTT_PORT;
+const int    mesh_port        = MESH_PORT;
+
+
+String ID  = String(ESP.getChipId());
+
+
+const char *out_topic = "esp8266-out/";
+const char *in_topic = "esp8266-in/";
+
+
+
+
+unsigned long previousMillis = 0;
+const long interval = 5000;
+int cnt = 0;
+
+
+ESP8266MQTTMesh mesh(FIRMWARE_ID, FIRMWARE_VER,
+                     networks, network_password, mesh_password,
+                     base_ssid, mqtt_server, mqtt_port, mesh_port,
+                     in_topic, out_topic);
+
+
+void callback(const char *topic, const char *msg);
+
+
+
+void setup() {
+
+    Serial.begin(115200);
+    mesh.setCallback(callback);
+    mesh.begin();
+    pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+
+void loop() {
+
+
+    mesh.loop();
+
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - previousMillis >= interval) {
+
+          String cntStr = String(cnt);
+          String msg = "hello from " + ID + " cnt: " + cntStr;
+          mesh.publish(ID.c_str(), msg.c_str());
+          previousMillis = currentMillis;
+          cnt++;
+      
+    }
+
+}
+
+
+
+void callback(const char *topic, const char *msg) {
+
+
+    if (0 == strcmp(topic, (const char*) ID.c_str())) {
+      if(String(msg) == "0") {
+        digitalWrite(LED_BUILTIN, HIGH);
+      }else{
+        digitalWrite(LED_BUILTIN, LOW);
+      }
+    }
+}
---
+++ examples/ESP8266MeshHelloWorld/credentials.h.example
@@ -0,0 +1,12 @@
+#define NETWORK_LIST { \
+             "ssid 1",     \
+             "ssid 2", \
+             "",            \
+             }
+#define NETWORK_PASSWORD "network password"
+#define MESH_PASSWORD    "esp8266_sensor_mesh"
+#define BASE_SSID        "mesh_esp8266-"
+#define MQTT_SERVER      "MQTT Server IP Address"
+#define MQTT_PORT        1883
+#define MESH_PORT        1884
+
