3a32c73c8917dad5894df00adfcd63e3702ca1c1
box25 <simon@klein-homepage.de>
2019-07-27 12:44:07 +0200

    readded the Option to configure Package Size in sendOTA

--- utils/send_ota.py
+++ utils/send_ota.py
@@ -4,7 +4,6 @@ import paho.mqtt.client as mqtt
 import os
 import sys
 import argparse
-import datetime
 import hashlib
 import base64
 import time
@@ -17,13 +16,11 @@ topic = "esp8266-"
 inTopic = topic + "in"
 outTopic = topic + "out"
 send_topic = ""
-name=""
-passw=""
+name = ""
+passw = ""
 q = queue.Queue()
+maxMQTTMessageLength = 768
 
-TIMEOUT_ERASE = 10
-TIMEOUT_PROGRAM = 5.0
-TIMEOUT_CHECK = 5
 
 def regex(pattern, txt, group):
     group.clear()
@@ -37,6 +34,7 @@ def regex(pattern, txt, group):
         return True
     return False
 
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
@@ -44,22 +42,23 @@ def on_connect(client, userdata, flags, rc):
     # reconnect then subscriptions will be renewed.
     client.subscribe("{}/#".format(outTopic))
 
+
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     #esp8266-out/mesh_esp8266-6/check=MD5 Passed
     match = []
-    if regex(r'/([0-9A-F]+)/ota/erase$', msg.topic, match):
+    if regex(r'/([0-9a-f]+)/ota/erase$', msg.topic, match):
         q.put(["erase", match[0]])
-    elif regex(r'([0-9A-F]+)/ota/md5/([0-9a-f]+)', msg.topic, match):
+    elif regex(r'([0-9a-f]+)/ota/md5/([0-9a-f]+)', msg.topic, match):
         q.put(["md5", match[0], match[1], msg.payload])
-    elif regex(r'([0-9A-F]+)/ota/check$', msg.topic, match):
+    elif regex(r'([0-9a-f]+)/ota/check$', msg.topic, match):
         q.put(["check", match[0], msg.payload])
     else:
-        #print("%s   %-30s = %s" % (str(datetime.datetime.now()), msg.topic, str(msg.payload)))
+        #print("%s   %-30s = %s" % (str(datetime.datetime.now()), msg.topic, str(msg.payload)));
         pass
-        
 
-def wait_for(nodes, msgtype, maxTime, retries=0, pubcmd=None, client=None):
+
+def wait_for(nodes, msgtype, maxTime):
     seen = {}
     origTime = time.time()
     startTime = origTime
@@ -74,43 +73,46 @@ def wait_for(nodes, msgtype, maxTime, retries=0, pubcmd=None, client=None):
         except queue.Empty:
             if time.time() - startTime < maxTime:
                 continue
-            if retries:
-                retries -= 1
-                print("{} node(s) missed the message, retrying".format(len(nodes) - len(seen.keys())))
-                client.publish(pubcmd[0], pubcmd[1])
-                startTime = time.time()
-            else:
-                if not nodes:
-                    break
-                print("{} node(s) missed the message and no retires left".format(len(nodes) - len(seen.keys())))
-                raise Exception('Send firmware', 'Device not connect')
-        if nodes and len(seen.keys()) == len(nodes):
+            if nodes:
+                print("{} node(s) missed the message".format(len(nodes) - len(seen.keys())))
+            break
+        if nodes and len(seen.keys()) == len(nodes): #if the Nodes are getting updated by their Module ID
             break
     #print("Elapsed time waiting for {} messages: {} seconds".format(msgtype, time.time() - origTime))
     return seen
+
 
 def send_firmware(client, data, nodes):
     md5 = base64.b64encode(hashlib.md5(data).digest())
     payload = "md5:%s,len:%d" %(md5.decode(), len(data))
     print("Erasing...")
     client.publish("{}start".format(send_topic), payload)
-    nodes = list(wait_for(nodes, 'erase', TIMEOUT_ERASE).keys())
+    nodes = list(wait_for(nodes, 'erase', 10).keys())
     if not nodes:
-        raise Exception('Find devices', 'Device not found')
+        print("No nodes responded to erase.  Aborting")
+        sys.exit(1)
     print("Updating firmware on the following nodes:\n\t{}".format("\n\t".join(nodes)))
     pos = 0
     while len(data):
-        d = data[0:768]
+        d = data[0:maxMQTTMessageLength]
         b64d = base64.b64encode(d)
-        data = data[768:]
-        client.publish("{}{}".format(send_topic, str(pos)), b64d)
+        data = data[maxMQTTMessageLength:]
+        topic = "{}{}".format(send_topic, str(pos))
+        client.publish(topic, b64d)
         expected_md5 = hashlib.md5(d).hexdigest().encode('utf-8')
+        retries = 0
         seen = {}
-        retries = 5
-        seen = wait_for(nodes, 'md5', TIMEOUT_PROGRAM, retries, ["{}{}".format(send_topic, str(pos)), b64d], client)
+        while True:
+            seen.update(wait_for(nodes, 'md5', 10.0))
+            if len(seen.keys()) == len(nodes):
+                break
+            if retries == 0:
+                break
+            client.publish(topic, b64d)
+            retries -= 1
         for node in nodes:
             if node not in seen:
-                print("No MD5 found for {} at 0x{}".format(node, pos))
+                print("No MD5 found for {} at 0x{} (expected {})".format(node, pos, expected_md5))
                 return
             addr = int(seen[node][2], 16)
             md5 = seen[node][3]
@@ -118,22 +120,24 @@ def send_firmware(client, data, nodes):
                 print("Got unexpected address 0x{} (expected: 0x{}) from node {}".format(addr, pos, node))
                 return
             if md5 != expected_md5:
-                print("Got unexpected md5 for node {} at 0x{}".format(node, addr))
+                print("Got unexpected md5 for node {} at 0x{},\n"
+                      "maybe the send Packages are to large for your MCU, if this happens every time try using the Argument: --packageLength".format(node, addr))
                 print("\t {} (expected: {})".format(md5, expected_md5))
                 return
         pos += len(d)
-        if pos % (768 * 13) == 0:
-            print("Transmitted %d bytes" % (pos))
+        if pos % (int(10000/maxMQTTMessageLength)*maxMQTTMessageLength) == 0: #aproximately every 10kb of send Data
+            print("Transmitted %d bytes" % pos)
     print("Completed send")
     client.publish("{}check".format(send_topic), "")
-    seen = wait_for(nodes, 'check', TIMEOUT_CHECK)
+    seen = wait_for(nodes, 'check', 5)
     err = False
     for node in nodes:
         if node not in seen:
             print("No verify result found for {}".format(node))
             err = True
         if seen[node][2] != b'MD5 Passed':
-            print("Node {} did not pass final MD5 check: {}".format(node, seen[node][2]))
+            print("Node {} did not pass final MD5 check: {},\n"
+                  "maybe the send Packages are to large for your MCU, if this happens every time try using the Argument: --packageLength".format(node, seen[node][2]))
             err = True
     if err:
         return
@@ -141,7 +145,7 @@ def send_firmware(client, data, nodes):
     client.publish("{}flash".format(send_topic), "")
 
 def main():
-    global inTopic, outTopic, name, passw, send_topic
+    global inTopic, outTopic, name, passw, send_topic, maxMQTTMessageLength
     parser = argparse.ArgumentParser()
     parser.add_argument("--bin", help="Input file");
     parser.add_argument("--id", help="Firmware ID (n HEX)");
@@ -150,11 +154,16 @@ def main():
     parser.add_argument("--user", help="MQTT broker user");
     parser.add_argument("--password", help="MQTT broker password");
     parser.add_argument("--ssl", help="MQTT broker SSL support");
-    parser.add_argument("--topic", help="MQTT mesh topic base (default: {}".format(topic))
-    parser.add_argument("--intopic", help="MQTT mesh in-topic (default: {}".format(inTopic))
-    parser.add_argument("--outtopic", help="MQTT mesh out-topic (default: {}".format(outTopic))
-    parser.add_argument("--node", help=("Specific node to send firmware to"))
+    parser.add_argument("--topic", help="MQTT mesh topic base (default: {})".format(topic))
+    parser.add_argument("--intopic", help="MQTT mesh in-topic (default: {})".format(inTopic))
+    parser.add_argument("--outtopic", help="MQTT mesh out-topic (default: {})".format(outTopic))
+    parser.add_argument("--node", help="Specific node to send firmware to")
+    parser.add_argument("--packageLength", help="Max ESP Payload Length, lower when always MD5 Mismatch (default: {})".format(outTopic))
     args = parser.parse_args()
+
+    if args.packageLength:
+        maxMQTTMessageLength = int(args.packageLength)
+
     if not os.path.isfile(args.bin):
         print("File: " + args.bin + " does not exist")
         sys.exit(1)
@@ -196,15 +205,17 @@ def main():
     client.on_connect = on_connect
     client.on_message = on_message
 
-    client.connect(args.broker, args.port, 60)
+    client.connect(args.broker, int(args.port), 60)
     client.loop_start()
 
     fh = open(args.bin, "rb")
-    data = fh.read();
+    data = fh.read()
     fh.close()
 
     send_firmware(client, data, [args.node] if args.node else [])
 
     client.loop_stop()
     client.disconnect()
+
+
 main()
