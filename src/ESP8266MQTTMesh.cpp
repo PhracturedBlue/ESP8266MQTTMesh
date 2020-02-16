8d2a8ebf12447b37ea85cd15cb749b9cef5709c4
box25 <simon@klein-homepage.de>
2020-02-17 00:38:00 +0100

    fix

--- src/ESP8266MQTTMesh.cpp
+++ src/ESP8266MQTTMesh.cpp
@@ -53,7 +53,7 @@ enum {
 
 #define dbgPrintln(lvl, msg)               \
     if (((lvl) & (EMMDBG_LEVEL)) == (lvl)) \
-    Serial.println("[" + String(__FUNCTION__) + "]" + msg);
+    Serial.println("[" + "" + "]" + msg); //String(__FUNCTION__)
 
 size_t mesh_strlcat(char* dst, const char* src, size_t len)
 {
