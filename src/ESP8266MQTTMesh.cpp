2906be9cdd928cbd9217b9c792c5402b237c2339
box25 <simon@klein-homepage.de>
2020-02-17 09:58:35 +0100

    little Fix

--- src/ESP8266MQTTMesh.cpp
+++ src/ESP8266MQTTMesh.cpp
@@ -657,9 +657,10 @@ bool ESP8266MQTTMesh::send_message(int index, const char *topicOrMsg, const char
         completeMessage += String("=").c_str();
         completeMessage += String(msg).c_str();
     }
-    char c_string[completeMessage.length() + 1];
+    char c_string[completeMessage.length() + 2];
     strcpy(c_string, completeMessage.c_str());
     c_string[completeMessage.length()] = '\n';
+    c_string[completeMessage.length() + 1] = '\0';
     espClient[index]->write(c_string);
     dbgPrintln(EMMDBG_WIFI_EXTRA, String("now sending raw Message: ") + c_string);
     return true;
